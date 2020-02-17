3dba7745e10bc9556932e8fb4b3b275947b8a891
Mihail Kachanovskii <nulay@mail.ru>
2020-02-17 15:50:28 +0800

    Some log

--- register.js
+++ register.js
@@ -43,8 +43,10 @@ exports.registerSubmit = function (req, resp) {
             // Get password from post data.
             var password = post_data["password"];
 
+consol.log(user_name+":"+password);
+
             // If user name and password is correct.
-            var curent_user = user(user_name,password);
+            var curent_user = user.user(user_name,password);
             
 if(!user.is_user_name(curent_user))
             {
