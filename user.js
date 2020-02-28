562bd9b1f5ebff8a2f02d7c359d07d66b2c341e8
Mihail Kachanovskii <nulay@mail.ru>
2020-02-28 12:44:42 +0800

    Rem global

--- user.js
+++ user.js
@@ -46,7 +46,7 @@ function readuserfromfile(){
    fs.readFile('users.json', (err, data) => {
      if (err) throw err;
      users = JSON.parse(data);
-     console.log('reading is executed count records are ' +global.users.length );
+     console.log('reading is executed count records are ' + users.length );
    });
 }
 
