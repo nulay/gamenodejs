a3e12da953924d338f5fd5e85640599382a18ec2
Mihail Kachanovskii <nulay@mail.ru>
2020-02-28 17:25:12 +0800

    G

--- app.js
+++ app.js
@@ -108,7 +108,7 @@ app.route('/login')
 // route for user's dashboard
 app.get('/dashboard', (req, res) => {
     if (req.session.user && req.cookies.user_sid) {
-         fs.readFile("/dashboard.html", "utf8", function(error, data){
+         fs.readFile("./dashboard.html", "utf8", function(error, data){
                  
         let message = "Hello "+req.session.user.name+"!"; 
          console.log(message);
