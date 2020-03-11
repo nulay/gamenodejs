1ce8c9335f67928824864d5af211afb7c5e598f0
Mihail Kachanovskii <nulay@mail.ru>
2020-03-11 23:05:24 +0800

    Crearte data for game

---
+++ game/model/dataforgame.js
@@ -0,0 +1,15 @@
+class DataForGame {
+   // private UserRoom userRoom;
+   // private List<ActionUser> listAction;
+   // private Set availAction;
+   // private Auction auction;
+
+    public DataForGame(userRoom, availAction, listAction, auction) {
+        this.userRoom = userRoom;
+        this.listAction = listAction;
+        this.availAction=availAction;
+        this.auction=auction;
+    }
+}
+
+module.exports = DataForGame;
