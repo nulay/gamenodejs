4aea54387309df20a3a074453df86c24ce79d1a9
Mihail Kachanovskii <nulay@mail.ru>
2020-03-11 23:24:20 +0800

    Create actionuser.js

---
+++ game/model/actionuser.js
@@ -0,0 +1,30 @@
+class ActionUser {
+    //private Date dateAction;
+    //private ActionMonopolyE action;
+    //private Object infoAction;
+    //private UserMonopoly user;
+    //--private Auction auction;
+    //GameMonopoly
+
+    createInstance(monopolyGame, user, action, infoAction){
+        var au=new ActionUser(user,action,infoAction);
+        populateUserByActions(monopolyGame.listUser);
+        populateUserByActions(monopolyGame.listViewUser);       
+    }
+
+    populateUserByActions(listUser){
+        for(var i=0;i<listUser.length;i++){
+            um = monopolyGame.listUser[i];
+            um.addActionUser(au);
+        }
+    }
+
+    constructor(user, action, infoAction) {
+        this.user=user;
+        this.action = action;
+        this.infoAction = infoAction;
+        this.dateAction=new Date();
+    }
+}
+
+module.exports = ActionUser;
