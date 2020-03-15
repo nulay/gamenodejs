0e5335b889f0e7b1ce5b6dff50398927191e317c
Mihail Kachanovskii <nulay@mail.ru>
2020-03-15 12:07:17 +0800

    Create carddefault.js

---
+++ game/model/carddefault.js
@@ -0,0 +1,59 @@
+class CardDefault{
+    //card name
+    //private String name;
+    var name;
+    //path to image card
+    //private String 
+    var image;
+    var type;
+
+    /**
+     * Default constructor for card
+     */
+    constructor() {
+    }
+
+    /**
+     * Method return Name card
+     * @return name card
+     */
+    //public String getName() {
+    getName() {
+        return this.name;
+    }
+
+    /**
+     * Method set name card
+     * @param name - name card
+     */
+    //public void setName(String name) {
+    setName(name) {
+        this.name = name;
+    }
+
+    /**
+     * Method return path to image card
+     * @return path to image card
+     */
+    //public String getImage() {
+    getImage() {
+        return this.image;
+    }
+
+    /**
+     * Method set for path to image card
+     * @param image path to image card
+     */
+    //public void setImage(String image) {
+    setImage(image) {
+        this.image = image;
+    }
+
+    getType() {
+        return this.type;
+    }
+    
+    setType(type) {
+        this.type = type;
+    }
+}
