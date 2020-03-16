ceb7c7f3a0dc0449d648cfecdf064ec181f0455d
Mihail Kachanovskii <nulay@mail.ru>
2020-03-16 13:53:44 +0800

    Update util.js

--- game/model/util.js
+++ game/model/util.js
@@ -2,5 +2,21 @@ class Util{
   static getRandom(min,max){
        return min + Math.floor((max - min) * Math.random());
   }
+  static addUnicAll(mainArr, additArr){
+       //Array.prototype.push.apply(mainArr, additArr);
+       for(const ell : additArr){
+          if(!isElementInArray(ell, mainArr){
+             mainArr[mainArr.length] = ell;
+          }
+       }
+  }
+  static isElementInArray(element, arr){
+       for(const ell : arr){
+         if(ell == element){
+            return true;
+         }
+       }
+       return false;
+  }
 }
 module.exports = Util;
