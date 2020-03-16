6b06bc5034b9fe5a8d42a0868f6ca659c0ef0c85
Mihail Kachanovskii <nulay@mail.ru>
2020-03-16 20:44:31 +0800

    Create gamesettings.js

---
+++ game/model/gamesettings.js
@@ -0,0 +1,12 @@
+var MonopolyGame = require("./monopolygame");
+var CardFirm = require("./cardfirm");
+
+
+class GameSettings(){
+    static createStandartMonopoly(roomForJoin){
+    var listCard =[new CardFirm('Мягков', 'firm', 1500,5,700,1,2,'firmR.png' ),
+                   new CardFirm('Мягков2', 'firm', 1500,5,700,1,2,'firmR.png' )];
+       var monopolyGame=new MonopolyGame(listCard, 3000, 1000, roomForJoin);
+       return monopolyGame;
+    }
+}
