3914834c2661f0b87439e8320d4ac76917c43514
Mihail Kachanovskii <nulay@mail.ru>
2020-03-16 23:41:09 +0800

    Create cardcheating.js

---
+++ game/model/cardcheating.js
@@ -0,0 +1,50 @@
+var CardDefault = require("./carddefault");
+class CardCheating extends CardDefault {
+    //private int numPrison;
+    numPrison;
+    /**
+     * Constructor with name and count steps for skip
+     * @param name - name
+     * @param numPrison - count step need skip
+     */
+    public CardCheating(name, numPrison){
+        setName(name, 'CardCheating');
+        this.numPrison=numPrison;       
+    }
+
+    /**{@inheritDoc}*/
+    //@Override
+    //public void transferCardForUser(Room room, UserRoom userRoom) {
+    transferCardForUser(gameMonopoly, userMonopoly) {
+        
+        userMonopoly.setPrison(1);
+        userMonopoly.setPenalty(gameMonopoly.getListCard()[this.numPrison].getPenalty());
+        userMonopoly.setIndexPosition(numPrison);
+        ActionUser.createInstance(gameMonopoly, userMonopoly, GO_PRISON, userMonopoly);
+    }
+
+    /**{@inheritDoc}*/
+    //@Override
+    //public void dropInToCard(Room room, UserRoom userRoom) {
+    dropInToCard(room, userRoom) {
+    }
+
+    /**
+     * Count step need skip
+     * @return count step need skip
+     */
+    //public int getNumPrison() {
+    getNumPrison() {
+        return this.numPrison;
+    }
+
+    /**
+     * Set count step need skip
+     * @param numPrison set count step need skip
+     */
+    //public void setNumPrison(int numPrison) {
+    setNumPrison(numPrison) {
+        this.numPrison = numPrison;
+    }
+}
+module.exports = CardCheating;
