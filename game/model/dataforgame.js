0e18e1a47ce53f5aa6956a85eb4d444958d2039a
Mihail Kachanovskii <nulay@mail.ru>
2020-03-20 16:09:22 +0800

    Update dataforgame.js

--- game/model/dataforgame.js
+++ game/model/dataforgame.js
@@ -4,12 +4,12 @@ class DataForGame {
    // private Set availAction;
    // private Auction auction;
 
-    constructor(userRoom, availAction, listAction, auction) {
+    constructor(userRoom, availAction, listAction, auction, listUser) {
         this.userRoom = userRoom;
         this.listAction = listAction;
         this.availAction=availAction;
         this.auction=auction;
-        
+        this.listUser=listUser;
     }
 }
 
