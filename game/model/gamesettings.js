20932ee54f769fc97223692ae6f4921a37be9b3f
Mihail Kachanovskii <nulay@mail.ru>
2020-03-23 18:05:18 +0800

    Update gamesettings.js

--- game/model/gamesettings.js
+++ game/model/gamesettings.js
@@ -48,7 +48,7 @@ class GameSettings{
            var mUser = new UserMonopoly(oldUser, 5, startMoney);          
            listUsers[listUsers.length]=mUser;
         }
-       var monopolyGame=new MonopolyGame(listCard, startMoney, moneyForCircle, roomForJoin.nameRoom, roomForJoin.maxCountUser listUsers);
+       var monopolyGame=new MonopolyGame(listCard, startMoney, moneyForCircle, roomForJoin.nameRoom, roomForJoin.maxCountUser, listUsers);
        monopolyGame.setImageFolder("images/monopoly");
        monopolyGame.setImageCenter("center.jpg");
        monopolyGame.credit=10000;
