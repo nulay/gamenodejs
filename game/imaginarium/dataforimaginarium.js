a9265f32f9ccca35f367fbc054d59b4a2a7716bd
Mihail Kachanovskii <nulay@mail.ru>
2020-03-30 15:20:36 +0300

    Create dataforimaginarium.js

---
+++ game/imaginarium/dataforimaginarium.js
@@ -0,0 +1,11 @@
+
+class DataForImaginarium {
+    constructor(userRoom, availAction, listAction, auction, listUser) {
+        this.userRoom = userRoom;
+        this.listAction = listAction;
+        this.availAction=availAction;
+        this.listUser=listUser;
+    }
+}
+
+module.exports = DataForImaginarium;
