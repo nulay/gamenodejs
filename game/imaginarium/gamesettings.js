605e5430535650d1790bec6b9f3037def480b5cf
Mihail Kachanovskii <nulay@mail.ru>
2020-03-30 13:30:28 +0300

    Create gamesettings.js

---
+++ game/imaginarium/gamesettings.js
@@ -0,0 +1,34 @@
+var ImaginariumGame = require("./monopolygame");
+//var CardFirm = require("./cardfirm");
+//var CardPlusMinus = require("./cardplusminus");
+//var CardPrison = require("./cardprison");
+//var CardCheating = require("./cardcheating");
+var UserImaginarium = require("./usermonopoly");
+
+class GameSettings{
+    static createStandartGame(roomForJoin){
+       console.log('createStandartImaginarium : '+JSON.stringify(roomForJoin));
+       var listCard =[new CardPlusMinus('Старт', [5000], '01.jpg'),
+                      new CardFirm('Мягков', 1500, 5, 700, 1, 2, 'firmR.png')
+                      ];
+        var listUsers = [];
+        
+        for(var i=0;i<roomForJoin.roomUsers.length;i++){       
+           var oldUser = roomForJoin.roomUsers[i];
+           var mUser = new UserMonopoly(oldUser, 5);          
+           listUsers[listUsers.length]=mUser;
+        }
+       var imaginariumGame=new ImaginariumGame(listCard, roomForJoin.nameRoom, roomForJoin.maxCountUser, listUsers);
+       imaginariumGame.setImageFolder("images/monopoly");
+       imaginariumGame.setImageCenter("center.jpg");
+       
+       imaginariumGame.setPenalty_cheating(10000);
+     
+       
+       imaginariumGame.startGameF();
+       console.log(imaginariumGame);
+       return imaginariumGame;
+    }
+}
+
+module.exports = GameSettings; 
