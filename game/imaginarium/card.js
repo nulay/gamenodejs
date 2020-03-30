3213025e97f21269458536a5c0120465c858bc33
Mihail Kachanovskii <nulay@mail.ru>
2020-03-30 15:05:56 +0300

    Create card.js

---
+++ game/imaginarium/card.js
@@ -0,0 +1,30 @@
+var CardDefault = require("./carddefault");
+var Util = require("./../../../model/util");
+var ActionUser = require("./actionuser");
+class Card extends CardDefault {
+    
+    //private int id;
+    id;
+    
+    userOwner;
+    
+    constructor(name, image){
+        super(name,"CARD",image);
+        this.price = price; 
+    }
+
+    getId() {
+        return this.id;
+    }
+
+    getUserOwner() {
+        return this.userOwner;
+    }
+
+    setUserOwner( userOwner) {
+        this.userOwner = userOwner.getName();
+    }
+
+}
+
+module.exports = Card;
